2db58f207aac4171b4f7e019c327ca834f58b1b1
jamiemharper98 <jamiemharper98@gmail.com>
2023-12-12 09:59:45 +0000

    using dayjs

--- components/CommentCard.jsx
+++ components/CommentCard.jsx
@@ -1,12 +1,15 @@
 import dayjs from "dayjs";
+import * as relativeTime from "dayjs/plugin/relativeTime";
+import upArrow from "../assets/icons/arrow-up.svg";
+dayjs.extend(relativeTime);
 
 export default function CommentCard({ comment }) {
   return (
     <section className="comment-card">
-      <p className="cc-author">{comment.author}</p>
+      <p>{comment.author}</p>
       <p className="cc-date">{dayjs().to(dayjs(comment.created_at))}</p>
       <p className="cc-body">{comment.body}</p>
-      <p className="cc-votes">
+      <p>
         {comment.votes}
         <button className="button-circle">
           <img src={upArrow} className="arrow-up" />
